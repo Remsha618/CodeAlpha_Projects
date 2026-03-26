2c4155e1dae60806f1dd68306054d3b09dc337d0
Remsha Imtiaz <zainab74malik747@gmail.com>
2026-03-26 22:36:27 +0500

    Create Task3_Login.py

---
+++ Task3_Login.py
@@ -0,0 +1,6 @@
+Username = input("Enter Your username : ")
+Password = input("Enter Your password : ")
+if Username == "Remsha Imtiaz" and Password == "0123" :
+    print("Login Successful")
+else:
+    print ("Invalid Login") 
